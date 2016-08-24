208155c8c86d244fab10b09016d6b7ef4b689548
Todd Schiller <todd.schiller@gmail.com>
2016-08-24 13:20:25 -0400

    Add WhiteNoise setup to wsgi

--- openintel/settings.py
+++ openintel/settings.py
@@ -126,7 +126,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
--- openintel/wsgi.py
+++ openintel/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+# https://devcenter.heroku.com/articles/django-assets
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openintel.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
