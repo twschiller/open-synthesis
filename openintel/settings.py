5a6fe7ca7d7723ca374b20910e5225b228fe9ca5
Todd Schiller <todd.schiller@gmail.com>
2016-08-24 12:59:28 -0400

    Namespace the secret key environment variable

--- openintel/settings.py
+++ openintel/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
