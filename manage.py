204eb8db1f512194c3dd101d4caa4764ef85ffa1
Todd Schiller <todd.schiller@gmail.com>
2016-08-26 14:11:13 -0400

    Try configuring coveralls code coverage report

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openintel.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
+    except ImportError:  # pragma: no cover
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
