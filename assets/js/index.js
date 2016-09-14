12b0f6156ee5a232f808df35e43493c65fdf9e54
Todd Schiller <todd.schiller@gmail.com>
2016-09-14 15:32:42 -0400

    #1: switch to double-quotes in JS files

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,9 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-require('jquery');
-require('bootstrap');
-require('bootstrap-datepicker');
+require("jquery");
+require("bootstrap");
+require("bootstrap-datepicker");
 
-require('bootstrap/dist/css/bootstrap.min.css');
-require('bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css');
+require("bootstrap/dist/css/bootstrap.min.css");
+require("bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css");
--- webpack.config.js
+++ webpack.config.js
@@ -16,20 +16,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 var path = require("path");
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     context: __dirname,
     // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
-    entry: './assets/js/index',
+    entry: "./assets/js/index",
     output: {
-        path: path.resolve('./assets/bundles/'),
+        path: path.resolve("./assets/bundles/"),
         filename: "[name]-[hash].js"
     },
     plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
+        new BundleTracker({filename: "./webpack-stats.json"}),
         // https://webpack.github.io/docs/list-of-plugins.html#provideplugin
         new webpack.ProvidePlugin({
             $: "jquery",
@@ -53,16 +53,16 @@ module.exports = {
         loaders: [
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file?name=public/fonts/[name].[ext]'
+                loader: "file?name=public/fonts/[name].[ext]"
             }
         ]
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'bower_components'],
-        extensions: ['', '.js', '.jsx']
+        modulesDirectories: ["node_modules", "bower_components"],
+        extensions: ["", ".js", ".jsx"]
     }
 };
