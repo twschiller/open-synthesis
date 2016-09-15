twschiller/open-synthesis

commit b5d83e8d7008f4b18800d8dd007c1e5abdb79752

closes #94: add EVIDENCE_REQUIRCE_SOURCE flag to control whether source is required when adding evidence; fix bug where the interface was requiring an event date

openach/views.py
@@ -22,6 +22,8 @@ from django.urls import reverse  # pylint: disable=no-name-in-module
 from django.core.exceptions import PermissionDenied, ImproperlyConfigured
 from django.conf import settings
 from django.views.decorators.http import require_http_methods, require_safe
+from django.forms import ValidationError
+from django.utils.translation import ugettext as _
 from slugify import slugify
 from field_history.models import FieldHistory
 
@@ -39,6 +41,7 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 PAGE_CACHE_TIMEOUT_SECONDS = 60
 DEBUG = getattr(settings, 'DEBUG', False)
 ACCOUNT_REQUIRED = getattr(settings, 'ACCOUNT_REQUIRED', False)
+EVIDENCE_REQUIRE_SOURCE = getattr(settings, 'EVIDENCE_REQUIRE_SOURCE', True)
 
 DEFAULT_EVAL = '------'
 KEEP_EVAL = '-- Keep Previous Assessment'
@@ -230,8 +233,9 @@ class EvidenceEditForm(forms.Form):
         help_text='A short summary of the evidence. Use the Event Date field for capturing the date'
     )
     event_date = forms.DateField(
-        label='Event Date',
+        label='Event Date (Optional)',
         help_text='The date the event occurred or started',
+        required=False,
         widget=forms.DateInput(attrs={'class': "date", 'data-provide': 'datepicker'})
     )
 
@@ -256,10 +260,22 @@ class BaseSourceForm(forms.Form):
 class EvidenceForm(BaseSourceForm, EvidenceEditForm):
     """Form for adding a new piece of evidence.
 
-    The evidence provided must have at least one source. The analyst can provide additional sources later.
+    If EVIDENCE_REQUIRE_SOURCE is set, the evidence provided must have a source. Analysts can provide additional
+    corroborating / conflicting sources after adding the evidence.
     """
 
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not EVIDENCE_REQUIRE_SOURCE:
+            self.fields['evidence_url'].required = False
+            self.fields['evidence_url'].label += ' (Optional)'
+            self.fields['evidence_date'].required = False
+            self.fields['evidence_date'].label += ' (Optional)'
+
+    def clean(self):
+        """Require a source date if the analyst provided a source URL"""
+        if self.cleaned_data['evidence_url'] and not self.cleaned_data['evidence_date']:
+            raise ValidationError(_('Please provide a date for the source.'), code='invalid')
 
 
 @require_http_methods(["HEAD", "GET", "POST"])
@@ -280,14 +296,15 @@ def add_evidence(request, board_id):
                     creator=request.user,
                     submit_date=submit_date
                 )
-                EvidenceSource.objects.create(
-                    evidence=evidence,
-                    source_url=form.cleaned_data['evidence_url'],
-                    source_date=form.cleaned_data['evidence_date'],
-                    uploader=request.user,
-                    corroborating=True,
-                    submit_date=submit_date
-                )
+                if form.cleaned_data['evidence_url'] and form.cleaned_data['evidence_date']:
+                    EvidenceSource.objects.create(
+                        evidence=evidence,
+                        source_url=form.cleaned_data['evidence_url'],
+                        source_date=form.cleaned_data['evidence_date'],
+                        uploader=request.user,
+                        corroborating=True,
+                        submit_date=submit_date
+                    )
 
             return HttpResponseRedirect(reverse('openach:detail', args=(board.id,)))
     else:

env.sample
@@ -16,3 +16,4 @@ SLUG_MAX_LENGTH=72
 ENABLE_CACHE=True
 ADMIN_EMAIL_ADDRESS="admin@localhost"
 ACCOUNT_REQUIRED=False
+EVIDENCE_REQUIRE_SOURCE=False

openintel/settings.py
@@ -58,6 +58,7 @@ env = environ.Env(  # pylint: disable=invalid-name
     ROLLBAR_ENABLED=(bool, False),
     ACCOUNT_REQUIRED=(bool, False),
     ACCOUNT_EMAIL_REQUIRED=(bool, True),
+    EVIDENCE_REQUIRE_SOURCE=(bool, True),
     INVITE_REQUIRED=(bool, False),
     SENDGRID_USERNAME=(str, None),
     SENDGRID_PASSWORD=(str, None),
@@ -305,6 +306,7 @@ if _detect_command('createadmin'):  # pragma: no cover
     ADMIN_PASSWORD = env('ADMIN_PASSWORD')
 ADMIN_EMAIL_ADDRESS = env('ADMIN_EMAIL_ADDRESS')
 INVITE_REQUIRED = env('INVITE_REQUIRED')
+EVIDENCE_REQUIRE_SOURCE = env('EVIDENCE_REQUIRE_SOURCE')
 
 # Authentication Options:
 # https://django-allauth.readthedocs.io/en/latest/configuration.html

commit 88b15779a2222d9e955d2263babb4541b61c70a0

#94: fix regression introduced in b5d83e8d7008f4b18800d8dd007c1e5abdb79752

openach/views.py
@@ -56,6 +56,11 @@ def check_owner_authorization(request, board, has_creator=None):
         raise PermissionDenied()
 
 
+def is_field_provided(form, field):
+    """Return true if field has non-None value in the form."""
+    return field in form.cleaned_data and form.cleaned_data[field] is not None
+
+
 @require_safe
 @account_required
 @cache_if_anon(PAGE_CACHE_TIMEOUT_SECONDS)
@@ -273,8 +278,8 @@ class EvidenceForm(BaseSourceForm, EvidenceEditForm):
             self.fields['evidence_date'].label += ' (Optional)'
 
     def clean(self):
-        """Require a source date if the analyst provided a source URL"""
-        if self.cleaned_data['evidence_url'] and not self.cleaned_data['evidence_date']:
+        """Require a source date if the analyst provided a source URL."""
+        if is_field_provided(self, 'evidence_url') and not is_field_provided(self, 'evidence_date'):
             raise ValidationError(_('Please provide a date for the source.'), code='invalid')
 
 
@@ -296,7 +301,7 @@ def add_evidence(request, board_id):
                     creator=request.user,
                     submit_date=submit_date
                 )
-                if form.cleaned_data['evidence_url'] and form.cleaned_data['evidence_date']:
+                if is_field_provided(form, 'evidence_url') and is_field_provided(form, 'evidence_date'):
                     EvidenceSource.objects.create(
                         evidence=evidence,
                         source_url=form.cleaned_data['evidence_url'],