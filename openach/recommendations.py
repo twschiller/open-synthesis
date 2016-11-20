364dca592c3f3d0ce8c94b8cdb0245cd859cd46c
chridam <chridam@mail.com>
2016-11-20 13:22:02 +0000

    #60: fix failing build checks

--- openach/recommendations.py
+++ openach/recommendations.py
@@ -13,11 +13,7 @@ from .models import Board, Evaluation
 
 
 class BoardRecommendationProvider(RecommendationProvider):
-    """ A class that specifies how to retrieve the various informations(boards, users, evaluations)
-        necessary for computing recommendations and similarities for a set of objects    
-    For more information, please see:
-        https://django-recommends.readthedocs.io/en/latest/providers.html
-    """
+    """ A class that specifies how to retrieve the various informations for recommendations and similarities"""
 
     @classmethod
     def get_users(self):
