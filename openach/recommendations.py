835f0d238b98a1a5c37a8a7277581dada8c839c2
chridam <chridam@mail.com>
2016-11-20 13:34:59 +0000

    #60: fix failing build checks

--- openach/recommendations.py
+++ openach/recommendations.py
@@ -12,36 +12,36 @@ from .models import Board, Evaluation
 
 
 class BoardRecommendationProvider(RecommendationProvider):
-    """ A class that specifies how to retrieve the various informations for recommendations and similarities"""
+    """A class that specifies how to retrieve the various informations for recommendations and similarities"""
 
     @classmethod
     def get_users(self):
-        """ Return all users with evaluations."""
+        """Return all users with evaluations."""
         return User.objects.filter(is_active=True, evaluations__isnull=False).distinct()
 
     @classmethod
     def get_items(self):
-        """ Return items that have been evaluated."""
+        """Return items that have been evaluated."""
         return Board.objects.all()
 
     @classmethod
     def get_ratings(self, obj):
-        """ Return all ratings (evaluations) for given item."""
+        """Return all ratings (evaluations) for given item."""
         return Evaluation.objects.filter(board=obj)
 
     @classmethod
     def get_rating_score(self, rating):
-        """ Return the rating score (evaluation value)."""
+        """Return the rating score (evaluation value)."""
         return rating.value
 
     @classmethod
     def get_rating_user(self, rating):
-        """ Return the user who performed the rating (evaluation)."""
+        """Return the user who performed the rating (evaluation)."""
         return rating.user
 
     @classmethod
     def get_rating_item(self, rating):
-        """ Return the rated (evaluated) object."""
+        """Return the rated (evaluated) object."""
         return rating.board
 
 recommendation_registry.register(Evaluation, [Board], BoardRecommendationProvider)
