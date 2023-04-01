2fe011736b2517cfcd5ef9de968f57288f15ec51
Todd Schiller <todd.schiller@gmail.com>
2023-03-31 23:06:34 -0400

    Try fixing memcachier on heroku

--- openintel/settings.py
+++ openintel/settings.py
@@ -426,23 +426,6 @@ def _get_cache():
                         "binary": True,
                         "username": env("MEMCACHIER_USERNAME"),
                         "password": env("MEMCACHIER_PASSWORD"),
-                        "behaviors": {
-                            # Enable faster IO
-                            "no_block": True,
-                            "tcp_nodelay": True,
-                            # Keep connection alive
-                            "tcp_keepalive": True,
-                            # Timeout settings
-                            "connect_timeout": 2000,  # ms
-                            "send_timeout": 750 * 1000,  # us
-                            "receive_timeout": 750 * 1000,  # us
-                            "_poll_timeout": 2000,  # ms
-                            # Better failover
-                            "ketama": True,
-                            "remove_failed": 1,
-                            "retry_timeout": 2,
-                            "dead_timeout": 30,
-                        },
                     },
                 }
             }
